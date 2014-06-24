f703eb0ebc198cb72d810efc77b16d1644c0b148
Alejandra Garcia-Rojas <alegrm@gmail.com>
2014-06-24 10:32:53 +0200

    add a function to fix update error

--- src/main/java/accounts/VirtuosoUserManager.java
+++ src/main/java/accounts/VirtuosoUserManager.java
@@ -13,8 +13,7 @@ public class VirtuosoUserManager implements UserManager {
   private Connection connection;
 
   /**
-   * This class manages the User creation in virtuoso for authentication and
-   * graph access control
+   * This class manages the User creation in virtuoso for authentication and graph access control
    * 
    * @param connectionString
    *          Connection string to Virtuoso
@@ -30,44 +29,60 @@ public class VirtuosoUserManager implements UserManager {
   }
 
   @Override
-  public void createUser(String name, String password) throws Exception {
+  public void createUser(String name, String password) throws ClassNotFoundException, SQLException {
     executeUpdate(getConnection(), "DB.DBA.USER_CREATE('" + name + "', '" + password + "')");
     // executeUpdate(getConnection(), "USER_SET_OPTION('" + name +
     // "', 'DAV_ENABLE', 1)");
   }
 
   @Override
-  public void dropUser(String name) throws Exception {
+  public void dropUser(String name) throws ClassNotFoundException, SQLException {
     executeUpdate(getConnection(), "USER_DROP('" + name + "', 1)");
   }
 
   @Override
-  public void grantRole(String user, String role) throws Exception {
+  public void grantRole(String user, String role) throws ClassNotFoundException, SQLException {
     executeUpdate(getConnection(), "GRANT " + role + " TO \"" + user + "\"");
   }
 
   @Override
-  public void setDefaultRdfPermissions(String user, int permissions) throws Exception {
+  public void setDefaultRdfPermissions(String user, int permissions) throws ClassNotFoundException,
+      SQLException {
     executeUpdate(getConnection(), "DB.DBA.RDF_DEFAULT_USER_PERMS_SET ('" + user + "', "
         + permissions + ")");
   }
 
   @Override
-  public void setRdfGraphPermissions(String user, String graph, int permissions) throws Exception {
+  public void setRdfGraphPermissions(String user, String graph, int permissions)
+      throws ClassNotFoundException, SQLException {
     executeUpdate(getConnection(), "DB.DBA.RDF_GRAPH_USER_PERMS_SET ('" + graph + "', '" + user
         + "', " + permissions + ")");
   }
 
   @Override
-  public void deleteRdfGraphPermissions(String user, String graph) throws Exception {
+  public void deleteRdfGraphPermissions(String user, String graph) throws ClassNotFoundException,
+      SQLException {
     executeUpdate(getConnection(), "DB.DBA.RDF_GRAPH_USER_PERMS_DEL ('" + graph + "', '" + user
         + "')");
   }
 
   @Override
-  public void setDefaultGraphPermissions(String graph, int permissions) throws Exception {
+  public void setDefaultGraphPermissions(String graph, int permissions)
+      throws ClassNotFoundException, SQLException {
     executeUpdate(getConnection(), "DB.DBA.RDF_GRAPH_USER_PERMS_SET ('" + graph + "', 'nobody', "
         + permissions + ")");
+  }
+
+  /**
+   * This function grant L_O_LOOK in virtuoso which was required to solve the error: Virtuoso 42000
+   * Error SR186: No permission to execute dpipe DB.DBA.L_O_LOOK with user ID 106, group ID 106
+   * 
+   * @param user
+   * @throws ClassNotFoundException
+   * @throws SQLException
+   */
+  public void grantLOLook(String user) throws ClassNotFoundException, SQLException {
+    executeUpdate(getConnection(), "GRANT EXECUTE ON DB.DBA.L_O_LOOK TO '" + user + "'");
   }
 
   private Connection getConnection() throws ClassNotFoundException, SQLException {
